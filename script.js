da2fe61eb58b025cf2ea3ca23643a0642ecf4958
vishnuviratGit <vishnuvirat564@gmail.com>
2025-03-26 20:15:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function makeid(l) {
   // write your code here
+	let res="";
+    let charList="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for(let i=0; i<l; i++){
+        res+=charList.charAt(Math.floor(Math.random()*charList.length));
+    }
+    return res;
+  
 }
+
 
 // Do not change the code below.
 const l = prompt("Enter a number.");
